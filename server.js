0e5cb9a5a831cc699a8160d3651d3c2b3888c915
Przemyslaw Symber <symber.przemyslaw@gmail.com>
2024-06-08 19:30:00 +0200

    Everything works

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const app = require('express');
-const server = app();
+const express = require('express');
+const path = require('path');
 
-server.get('/', (req, res) => {
-  res.send('Hello World');
-} );
+const app = express();
+app.use(express.static(path.join(__dirname, '/static')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/template/index.html'));
+});
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
-
